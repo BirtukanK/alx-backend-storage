8aa1643173e8151073ac807590aab59746d30bb9
BirtukanK <birtukankuma1113@gmail.com>
2024-05-10 20:29:06 +0000

    sql average wighted score

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Check if the table already exists before creating it
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,  -- Auto-incrementing integer primary key
+    email VARCHAR(255) NOT NULL UNIQUE,  -- Unique email address, up to 255 characters
+    name VARCHAR(255),   -- Name field, up to 255 characters
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
