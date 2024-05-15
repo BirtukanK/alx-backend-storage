ed627066137bf77f5dbe87a3048790296bc88813
BirtukanK <birtukankuma1113@gmail.com>
2024-05-15 12:41:55 +0000

    read file from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) ->
+    Union[str, bytes, int, float, None]:
+        """ Method to get value"""
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Method to get string values"""
+        return self.get(key, fn=lambda d: d.decode('utf-8')
+                        if isinstance(d, bytes) else d)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Method to get int values"""
+        return self.get(key, fn=lambda d: int(d)
+                        if isinstance(d, bytes) else d)
